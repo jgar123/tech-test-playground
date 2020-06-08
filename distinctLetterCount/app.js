52e84aaf29c038ae5a7922d77eac04dd628da145
Jonny Garius <gariusjonathan@hotmail.co.uk>
2020-06-08 12:12:34 +0100

    distinct

--- distinctLetterCount/app.js
+++ distinctLetterCount/app.js
@@ -1,9 +1,50 @@
 function distinctLetterCount(s) {
 
-  
-  
+  const splitString = s.split('')
+
+  const letterCounts = splitString.reduce((obj, char) => {
+    if (obj[char]) {
+      obj[char]++
+      return obj
+    } else {
+      obj[char] = 1
+      return obj
+    }
+  }, {})
+
+  const sortedString = splitString.filter((char, i) => splitString.indexOf(char) === i).sort()
+  const result = []
+
+  sortedString.forEach(letter => {
+
+    if (!result.includes(letterCounts[letter])) {
+      result.push(letterCounts[letter])
+    } else {
+      let decreasing = true
+      let count = letterCounts[letter] - 1
+      while (decreasing) {
+
+        if (count <= 0) {
+          decreasing = false
+        }
+        if (!result.includes(count)) {
+          result.push(count)
+          decreasing = false
+        } else {
+          count--
+        }
+
+      }
+    }
+
+  })
+
+  return result.reduce((acc, num) => acc + num)
+
 
 }
+
+module.exports = { distinctLetterCount }
 
 
 
--- distinctLetterCount/app.test.js
+++ distinctLetterCount/app.test.js
@@ -5,5 +5,10 @@ test('Expect the return the length of a string that contains distinct counts of 
   expect(distinctLetterCount('aabbccddeeffgg')).toBe(3)
   expect(distinctLetterCount('hello')).toBe(3)
   expect(distinctLetterCount('thesecanbetricky')).toBe(6)
+  expect(distinctLetterCount('example')).toBe(3)
+  expect(distinctLetterCount('eeee')).toBe(4)
+  expect(distinctLetterCount('bonnnoo')).toBe(6)
+  expect(distinctLetterCount('aabfgshhhhwtllla')).toBe(10)
+  expect(distinctLetterCount('bobsyouruncle')).toBe(3)
 })
 
